9a7fee7e75435a4f2ada68c80197616266c7d140
Himanchuz <65249066+Himanchuz@users.noreply.github.com>
2026-04-29 18:20:03 +0200

    Update esES.lua

--- MidnightWorld/Locales/esES.lua
+++ MidnightWorld/Locales/esES.lua
@@ -1,9 +1,9 @@
 local L = BigWigs:NewBossLocale("Thorm'belan", "esES")
 if not L then return end
 if L then
-	--L.ball = "Ball"
-	--L.ball_incoming = "Ball Incoming - Don't let it touch the ground"
-	--L.ball_fail = "FAIL - Ball touched the ground"
-	--L.tendrils = "Tendrils"
-	--L.tendrils_incoming = "RUN AWAY to snap tendrils"
+	L.ball = "Bola"
+	L.ball_incoming = "Bola cayendo - No dejes que toque el suelo"
+	L.ball_fail = "FALLO - La bola ha tocado el suelo"
+	L.tendrils = "Zarcillos"
+	L.tendrils_incoming = "ALÉJATE para romper los zarcillos"
 end
